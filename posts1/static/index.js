ddb3e017734e9e94e00bf72eb517451867cc347c
Saed Yousef <saed.alzaben@gmail.com>
2020-04-30 00:08:11 +0300

    Posts 0

---
+++ posts1/static/index.js
@@ -0,0 +1,47 @@
+// Start with first post
+let counter = 1;
+const quantity = 20;
+
+// When DOM load, render the first 20 posts.
+document.addEventListener("DOMContentLoaded", load);
+
+// If scrolled to bottom, load the next 20 posts.
+window.onscroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight)
+        load();
+}
+
+// Load next set of posts
+function load() {
+    // Set start and end post
+    const start = counter;
+    const end = start + quantity -1;
+    counter = end +1;
+
+    // Open new request to get new posts.
+    const request = new XMLHttpRequest();
+    request.open('POST', '/posts');
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        data.forEach(add_post);
+    }
+
+    // Add start and end points to request data.
+    const data = new FormData();
+    data.append('start', start);
+    data.append('end', end);
+
+    request.send(data);
+
+}
+
+function add_post(contents) {
+    // Create new post
+    const post = document.createElement('div');
+    post.className = 'post';
+    post.innerHTML = contents;
+
+    // Add post to DOM.
+    document.querySelector('#posts').append(post);
+}
+
